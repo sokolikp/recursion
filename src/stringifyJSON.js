52fc5eb3c0301e78183b10b2ba0fee8ded5791fa
Paul Sokolik <paul.sokolik@gmail.com>
2015-04-13 13:54:07 -0500

    'failing on objects containing functions'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  	  
+  if(obj === undefined || obj === null) {
+  	return "null";
+  }
+
+  else if(typeof(obj) === 'string') {
+  	return '\"' + obj + '\"';
+  }
+
+  else if(typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+  	return obj.toString();
+  }
+
+  else if( Array.isArray(obj) ) {
+  	if(obj.length === 0) {
+  		return '[]';
+  	}
+  	var returnString = '[';
+  	for(var i=0; i<obj.length-1; i++) {
+  	  returnString += (stringifyJSON(obj[i]) + ',');
+	}
+    returnString += stringifyJSON(obj[obj.length-1]);
+	returnString += ']';
+	return returnString;
+  }
+
+  else {
+  	if(Object.keys(obj).length === 0) {
+  		return '{}';
+  	}
+  	console.log(obj);
+	var returnString = '{';
+	for(var key in obj) {
+	  //console.log(obj);
+	  //console.log(obj.key);
+	  returnString += (stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',');
+	}
+	returnString = returnString.substring(0, returnString.length - 1);
+	returnString += '}';
+	//console.log(returnString);
+	return returnString;
+  }
+ // }
 };
