ecb939320ca3a4019474a1b2ff331111661e3640
Paul Sokolik <paul.sokolik@gmail.com>
2015-05-09 18:57:08 -0700

    'updated parseJSON; passing ~11 cases'

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,7 +4,8 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   if(typeof(json) !== 'string') {
-  	return SyntaxError('Error!');
+  	throw new SyntaxError('Error!');
+    //return -1;
   }
   else if(json === '[]') {
   	return [];
@@ -13,32 +14,84 @@ var parseJSON = function(json) {
   	return {};
   }
   else if (json[0] === '{') {
-  	var returnObj = {};
-  	var splitObj = json.split(': ');
-    splitObj[0] = splitObj[0].split('{')[1];
-    splitObj[splitObj.length-1] = splitObj[splitObj.length-1].split('}')[0];
-    for(var i=0; i<splitObj.length-1; i++) {
-      var keyStr = parseJSON(splitObj[i]);
-      if(splitObj[i+1].split(', "')[0] === splitObj[i+1]) {
-        var valStr = parseJSON(splitObj[i+1]);
-      }
-      else {
-        var valStr = parseJSON(splitObj[i+1].split(', "')[0]);
-        splitObj[i+1] = '"' + splitObj[i+1].split(', "')[1];
-      }    
-      
-      returnObj[keyStr] = valStr;
+    if(json[json.length-1] !== '}') {
+      throw new SyntaxError();
+      //return -1;
     }
-  	return returnObj;
+    if(json.split(':{')[0] !== json) {
+      var returnObj = {};
+      var splitObj = json.split(':{');
+      splitObj[0] = splitObj[0].split('{')[1];
+      splitObj[splitObj.length-1] = splitObj[splitObj.length-1].split('}')[0];
+      for(var i=0; i<splitObj.length-1; i++) {
+        var keyStr = parseJSON(splitObj[i].trim());
+        if(splitObj[i+1].split(', "')[0] === splitObj[i+1]) {
+          var valStr = parseJSON('{' + splitObj[i+1] + '}');
+          //var valStr = innerObj;
+          //var innerSplit = json.split()
+          //var innerObjValStr = parseJSON(splitObj[i+1].trim());
+        }
+        else {
+          var valStr = parseJSON(splitObj[i+1].split(', "')[0].trim());
+          splitObj[i+1] = '"' + splitObj[i+1].split(', "')[1];
+        }            
+        returnObj[keyStr] = valStr;
+      }
+      return returnObj;
+    }
+    else if(json.split(':[')[0] !== json) {
+      var returnObj = {};
+      var splitObj = json.split(':[');
+      splitObj[0] = splitObj[0].split('{')[1];
+      splitObj[splitObj.length-1] = splitObj[splitObj.length-1].split('}')[0];
+
+      for(var i=0; i<splitObj.length-1; i++) {
+        var keyStr = parseJSON(splitObj[i].trim());
+        if(splitObj[i+1].split('], "')[0] === splitObj[i+1]) {
+          var valStr = parseJSON('[' + splitObj[i+1]);
+          //var valStr = innerObj;
+          //var innerSplit = json.split()
+          //var innerObjValStr = parseJSON(splitObj[i+1].trim());
+        }
+        else {
+          var valStr = parseJSON(splitObj[i+1].split(', "')[0].trim());
+          splitObj[i+1] = '"' + splitObj[i+1].split(', "')[1];
+        }            
+        returnObj[keyStr] = valStr;
+      }
+      return returnObj;
+    }
+    else {
+  	  var returnObj = {};
+    	var splitObj = json.split(':');
+      splitObj[0] = splitObj[0].split('{')[1];
+      splitObj[splitObj.length-1] = splitObj[splitObj.length-1].split('}')[0];
+      for(var i=0; i<splitObj.length-1; i++) {
+        var keyStr = parseJSON(splitObj[i].trim());
+        if(splitObj[i+1].split(', "')[0] === splitObj[i+1]) {
+          var valStr = parseJSON(splitObj[i+1].trim());
+        }
+        else {
+          var valStr = parseJSON(splitObj[i+1].split(', "')[0].trim());
+          splitObj[i+1] = '"' + splitObj[i+1].split(', "')[1];
+        }            
+        returnObj[keyStr] = valStr;
+      }
+    	return returnObj;
+    }
   }
   else if(json[0] === '[') {
+    if(json[json.length-1] !== ']') {
+      throw new SyntaxError('Error!');
+      //return -1;
+    }
   	var returnObj = [];
   	var splitStr = json.split(',');
     splitStr[0] = splitStr[0].split('[')[1];
     splitStr[splitStr.length-1] = splitStr[splitStr.length-1].split(']')[0];
   	//grab only odd split values
   	for(var i=0; i<splitStr.length; i++) {
-      returnObj.push(parseJSON(splitStr[i]));
+      returnObj.push(parseJSON(splitStr[i].trim()));
   	}
     return returnObj;
   }
